9a191708c6a09f44b71ac83d5ed467c13f575ba0
strider99 <panigrahiomprakash@gmail.com>
2017-12-17 11:21:36 +0530

    add prompt for filepath and add proper error messages

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const readCSV = require('./util/Read-CSV');
+const rl = require("readline");
 
-// accept file path as argument
-let filePath = process.argv[2];
+let prompt = rl.createInterface(process.stdin, process.stdout);
 
-readCSV(filePath);
+// store passed parameters from command line
+const args = process.argv.slice(2);
+
+const startParsingFile = (args) => {
+  if(args.length === 0){
+    // prompt use for filename and separator
+    prompt.question("Enter the <filepath/filename separator>", function (params) {
+      params = params.split(' ');
+      if(params[0]){
+        readCSV(params[0]);
+        prompt.close();
+      }
+      else{
+        console.log("File Path or separator was not specified");
+        startParsingFile([]);
+      }
+    });
+  }
+  else{
+    readCSV(args[0]);
+    prompt.close();
+  }
+}
+
+
+// accept file path and separator as arguments
+startParsingFile(args);
+
 
--- util/Read-CSV.js
+++ util/Read-CSV.js
@@ -3,22 +3,26 @@ const fs = require('fs');
 const readCsv = (filePath) => {
 	const output = fs.readFile(filePath, 'utf-8', (err, data) => {
 		if(err){
-			console.warn('there was an error');
+			console.error('File was not found. Exiting...');
 		}
-		// store every row in an array 
-		const rows = data.trim().split(/\r?\n/);
-		 rows.forEach((row) => {
-			 let rowArray = row.split(',');
-			
-		 	if(rowArray.length === 3){
+		else {
+			// store every row in an array 
+			const rows = data.trim().split(/\r?\n/);
+			rows.forEach((row) => {
+				let rowArray = row.split(',');
 				
-		 		console.log(rowArray.join(','));
-		 	}
-		 	else {
-				  console.warn(`${rowArray.join(',')}` + '\x1b[33m%s\x1b[0m', ' !Data Not Found!');
-		 	}
-		 });
-	});
+				if(rowArray.length >= 3){
+					console.log(rowArray.join(','));
+				}
+				else {
+					console.warn(`${rowArray.join(',')}` + '\x1b[33m%s\x1b[0m', ' !Data Not Found!');
+				}
+			});
+		}
+			
+		});
+		
+	
 }
 
 module.exports = readCsv;
