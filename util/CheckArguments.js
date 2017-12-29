5b4cab27b495dfc63df5cb2d66182d6f8aac386c
strider99 <panigrahiomprakash@gmail.com>
2017-12-29 12:51:13 +0530

    store filepath, separator and format as arrays.

--- util/CheckArguments.js
+++ util/CheckArguments.js
@@ -9,10 +9,18 @@ class CheckArguments extends EventEmitter {
     })
   }
   check(args){
-    // console.log(args);
     let argsLength = args.length;
-    console.log(argsLength);
-    this.emit(eventsNames.events.AFTER_CHECK,args);
+    let parseParams = []; //store arrays of filepath, separator and file format
+    if(argsLength < 3){
+      console.log("Invalid Input format.");
+      process.exit();
+    }
+    else if(argsLength % 3 == 0){
+      for(let i = 0; i <= argsLength - 3; i = i + 3){
+        parseParams.push([args[i], args[i+1], args[i+2]]);
+      }
+      this.emit(eventsNames.events.AFTER_CHECK,parseParams);
+    }
   }
 
 }
